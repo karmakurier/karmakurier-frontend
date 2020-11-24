137cdb97a4f115aa6d651b00d857fd70caa9d2b8
bastisk <basti.sk@gmx.de>
2020-11-24 19:47:00 +0100

    changing paths to new routes

--- src/components/molecules/ContactForm/ContactForm.molecule.js
+++ src/components/molecules/ContactForm/ContactForm.molecule.js
@@ -13,7 +13,7 @@ class ContactForm extends React.Component {
             email: '',
             message: '',
             captcha: '',
-            captchasvg: `${process.env.REACT_APP_CONTACT_BASEURL}/captcha`,
+            captchasvg: `${process.env.REACT_APP_CONTACT_BASEURL}/contactapi-captcha`,
             captchaWrong: false,
             submitSuccess: false,
         }
@@ -32,7 +32,9 @@ class ContactForm extends React.Component {
 
     loadSvg() {
         const newDate = Date.now()
-        this.setState({ captchasvg: `${process.env.REACT_APP_CONTACT_BASEURL}/captcha?${newDate}` })
+        this.setState({
+            captchasvg: `${process.env.REACT_APP_CONTACT_BASEURL}/contactapi-captcha?${newDate}`,
+        })
     }
 
     handleSubmit() {
