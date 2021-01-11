ca3eea69eb242244e790e57da8649f4b06875ac8
bastisk <basti.sk@gmx.de>
2021-01-11 22:19:00 +0100

    fixes display issue with wron header height for contact page

--- src/components/pages/Contact/Contact.page.js
+++ src/components/pages/Contact/Contact.page.js
@@ -1,3 +1,4 @@
+import Container from '@material-ui/core/Container'
 import Grid from '@material-ui/core/Grid'
 import React from 'react'
 import { ReactComponent as ContactIcon } from '../../../assets/img/contactform/atom_undraw_kontakt.svg'
@@ -8,23 +9,21 @@ import './Contact.page.scss'
 const Contact = () => {
     return (
         <MainLayout>
-            <Grid
-                className="contactForm"
-                container
-                spacing={3}
-                justify="space-between"
-                alignItems="center"
-            >
-                <Grid className="headPicture" item xs={12} sm={5} align-content="stretch">
-                    <ContactIcon />
-                </Grid>
-                <Grid item xs={12} sm={7}>
-                    <div className="section">
-                        <h1>kontaktiere uns</h1>
-                        <ContactForm />
-                    </div>
-                </Grid>
-            </Grid>
+            <div className="contactForm">
+                <Container maxWidth="lg">
+                    <Grid container alignItems="center">
+                        <Grid className="headPicture" item xs={12} sm={5} align-content="stretch">
+                            <ContactIcon />
+                        </Grid>
+                        <Grid item xs={12} sm={7}>
+                            <div className="section">
+                                <h1>kontaktiere uns</h1>
+                                <ContactForm />
+                            </div>
+                        </Grid>
+                    </Grid>
+                </Container>
+            </div>
         </MainLayout>
     )
 }
